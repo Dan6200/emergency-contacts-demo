639a8b0a9cdb262810c00d6eb1510bfaff737e63
Daniel Nyong <danielnyong66@gmail.com>
2024-06-08 23:55:36 +0100

    fixed QR Functionality

--- components/qr/index.tsx
+++ components/qr/index.tsx
@@ -4,13 +4,12 @@ import { useEffect, useState } from "react";
 import { QrReader } from "react-qr-reader";
 import { CameraOff } from "lucide-react";
 import { useRouter } from "next/navigation";
+import debounce from "lodash.debounce";
 
 export default function QRFetchResidents() {
   const [camOn, setCamOn] = useState(false),
-    [camLoaded, setCamLoaded] = useState(false),
     [isQR, setIsQR] = useState(false),
-    [exists, setExists] = useState(false),
-    [fetchResErr, setFetchResErr] = useState(false);
+    [fetchResidentErr, setFetchResidentErr] = useState<string | null>(null);
   const router = useRouter();
   const Id = "qr-video";
   // Set Fallback elements when camera does not show
@@ -19,56 +18,66 @@ export default function QRFetchResidents() {
       .getUserMedia({
         video: { facingMode: "environment" },
       })
-      .then((stream) => setCamLoaded(stream.active));
-    setCamOn(camLoaded);
-  }, [camLoaded]);
+      .then((stream) => setCamOn(stream.active));
+  }, []);
   // Error Message to UI
   useEffect(() => {
-    toast({
-      title: "Unable To Fetch Resident Info",
-    });
-  }, [fetchResErr]);
+    let debouncedFunc: any;
+    if (fetchResidentErr) {
+      toast({
+        title: fetchResidentErr,
+      });
+      debouncedFunc = debounce(() => {
+        setIsQR(false);
+        setFetchResidentErr(null);
+      }, 1000);
+      debouncedFunc();
+    }
+    return () => debouncedFunc?.cancel();
+  }, [isQR, fetchResidentErr]);
   return (
     <main className="w-full px-4 md:w-1/2 mx-auto my-4">
-      {camOn && (
-        <>
-          <h1 className="text-3xl font-semibold">Scan Qr code</h1>
-          <QrReader
-            constraints={{ facingMode: "environment" }}
-            videoId={Id}
-            onResult={(result, error) => {
-              if (result) {
-                console.log(result);
-                setIsQR(true);
-                const ID = result.getText();
-                const url = new URL(`/resident/${ID}`);
-                fetch(url, { method: "HEAD" }).then((res) => setExists(res.ok));
-                if (exists) router.push(`/resident/${ID}`);
-                else setFetchResErr(true);
+      <h1 className="text-3xl font-semibold">Scan Qr code</h1>
+      {camOn ? (
+        <QrReader
+          constraints={{ facingMode: "environment" }}
+          videoId={Id}
+          onResult={(result, error) => {
+            if (result) {
+              setIsQR(true);
+              const ID = result.getText();
+              try {
+                const url = `/resident/${ID}`;
+                fetch(url, { method: "HEAD" }).then((res) => {
+                  if (res.ok) router.push(url.toString());
+                  if (res.status === 404)
+                    setFetchResidentErr("Resident Does Not Exist");
+                });
+              } catch (e) {
+                setFetchResidentErr("Failed to Retrieve Resident Info");
+                console.error(e);
               }
-              if (error) {
-                console.error(error);
-              }
-            }}
-            className={
-              (isQR ? "bg-green-700 " : "bg-red-600 ") +
-              "transition-colors border-2 rounded-lg p-4 sm:p-6 md:p-8 flex items-center bg-green-700 my-4 bg-black-800 mx-auto"
             }
-            containerStyle={{ height: "100%", width: "100%" }}
-            videoContainerStyle={{ height: "100%", width: "100%" }}
-            videoStyle={{
-              height: "100%",
-              width: "100%",
-              objectFit: "cover",
-              rounded: "10px",
-            }}
-          />
-        </>
-      )}
-      {!camOn && (
-        <div className="flex flex-col gap-5">
-          <CameraOff className="w-32 h-32 mx-auto" />
-          <p className="text-2xl capitalize">
+            if (error) {
+            }
+          }}
+          className={
+            (isQR ? "bg-green-700 " : "bg-red-600 ") +
+            "transition-colors border-2 rounded-lg p-4 sm:p-6 md:p-8 flex items-center bg-green-700 my-4 bg-black-800 mx-auto"
+          }
+          containerStyle={{ height: "100%", width: "100%" }}
+          videoContainerStyle={{ height: "100%", width: "100%" }}
+          videoStyle={{
+            height: "100%",
+            width: "100%",
+            objectFit: "cover",
+            rounded: "10px",
+          }}
+        />
+      ) : (
+        <div className="flex flex-col gap-5 border-4 my-4 p-4 sm:p-6 border-primary/80 md:p-8 h-[40vh] w-full rounded-md">
+          <CameraOff className="w-28 h-28 mx-auto" />
+          <p className="text-xl capitalize">
             Camera is off. Please turn on camera and refresh to scan QR code
           </p>
         </div>
