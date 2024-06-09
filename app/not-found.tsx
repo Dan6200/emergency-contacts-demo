1eb5075794268ce0a3d3968a8fb1ab5eabb0e938
Daniel Nyong <danielnyong66@gmail.com>
2024-06-09 09:38:07 +0100

    updated notfound page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,7 +8,7 @@ export default function NotFound() {
     setTimeout(() => router.push("/"), 1000);
   }, []);
   return (
-    <main className="bg-background font-semibold text-center mx-auto py-8">
+    <main className="bg-background font-semibold w-4/5 text-center mx-auto py-8">
       <h1 className="text-4xl mb-4">LinkID</h1>
       <p className="text-3xl">This page does not exist</p>
     </main>
