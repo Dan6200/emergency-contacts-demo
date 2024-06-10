65cafd2c22dc923e048ea094285687149a6ee4ce
Daniel Nyong <danielnyong66@gmail.com>
2024-06-10 13:45:08 +0100

    fix error

--- app/admin/residents/search/page.tsx
+++ app/admin/residents/search/page.tsx
@@ -1,13 +1,11 @@
 import Search from "@/components/residents/search";
-import { getAuthenticatedAppForUser } from "@/server";
 export default async function SearchResidents() {
-  const { currentUser: initialUser } = await getAuthenticatedAppForUser();
   return (
     <main className="bg-background text-center mx-auto py-8">
       <h1 className="text-2xl font-semibold w-4/5 mx-auto">
         Search Resident Information
       </h1>
-      <Search {...{ initialUser }} />
+      <Search />
     </main>
   );
 }
