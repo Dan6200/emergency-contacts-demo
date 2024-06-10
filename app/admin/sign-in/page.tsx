9b4f9cf73ad2e78e0845122c5e45066d9f2c6085
Daniel Nyong <danielnyong66@gmail.com>
2024-06-10 14:06:09 +0100

    fix errors

--- app/admin/sign-in/page.tsx
+++ app/admin/sign-in/page.tsx
@@ -1,11 +1,9 @@
 import { SignInForm } from "@/components/sign-in-form/";
-import { getAuthenticatedAppForUser } from "@/server";
 
 export default async function SignInPage() {
-  const { currentUser: initialUser } = await getAuthenticatedAppForUser();
   return (
     <main className="bg-background container w-full md:w-2/3 mx-auto my-16 max-h-screen">
-      <SignInForm {...{ initialUser }} />
+      <SignInForm />
     </main>
   );
 }
