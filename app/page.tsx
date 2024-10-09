7490fa29613e2d9b0a2535e5ee84b1a61b18c8f6
Daniel Nyong <danielnyong66@gmail.com>
2024-10-09 13:36:19 +0100

    updates to ui

--- app/page.tsx
+++ app/page.tsx
@@ -20,46 +20,48 @@ export default async function Home() {
   });
   return (
     <main className="sm:container bg-background text-center mx-auto py-32 sm:py-24">
-      <Table className="text-base w-4/5 mx-auto">
-        <TableCaption>All Rooms In The Facility.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="text-center md:w-48">Keyword</TableHead>
-            <TableHead className="text-center md:w-48">Room</TableHead>
-            <TableHead className="text-left">Address</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {rooms?.map(
-            ({
-              id,
-              roomNo,
-              residence_id,
-              address,
-            }: Residence & { id: string }) => {
-              return (
-                <TableRow key={id}>
-                  <TableCell className="text-center">
-                    <Link href={`/room/${id}`} className="w-full block">
-                      {residence_id}
-                    </Link>
-                  </TableCell>
-                  <TableCell className="text-center">
-                    <Link href={`/room/${id}`} className="w-full block">
-                      {roomNo}
-                    </Link>
-                  </TableCell>
-                  <TableCell className="text-left">
-                    <Link href={`/room/${id}`} className="w-full block">
-                      {address}
-                    </Link>
-                  </TableCell>
-                </TableRow>
-              );
-            }
-          )}
-        </TableBody>
-      </Table>
+      <div className="w-fit rounded-md border-2 mx-auto">
+        <Table className="text-base w-full">
+          <TableCaption>All Rooms In The Facility.</TableCaption>
+          <TableHeader className="bg-foreground/20 font-bold rounded-md">
+            <TableRow>
+              <TableHead className="text-center md:w-48">Keyword</TableHead>
+              <TableHead className="text-center md:w-48">Room</TableHead>
+              <TableHead className="text-left">Address</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {rooms?.map(
+              ({
+                id,
+                roomNo,
+                residence_id,
+                address,
+              }: Residence & { id: string }) => {
+                return (
+                  <TableRow key={id}>
+                    <TableCell className="text-center">
+                      <Link href={`/room/${id}`} className="w-full block">
+                        {residence_id}
+                      </Link>
+                    </TableCell>
+                    <TableCell className="text-center">
+                      <Link href={`/room/${id}`} className="w-full block">
+                        {roomNo}
+                      </Link>
+                    </TableCell>
+                    <TableCell className="text-left">
+                      <Link href={`/room/${id}`} className="w-full block">
+                        {address}
+                      </Link>
+                    </TableCell>
+                  </TableRow>
+                );
+              }
+            )}
+          </TableBody>
+        </Table>
+      </div>
     </main>
   );
 }
