d74d5c52120c38571ca466835ddc2a13c3eaa6d7
Daniel Nyong <danielnyong66@gmail.com>
2024-10-10 15:22:53 +0100

    bugfix

--- components/room.tsx
+++ components/room.tsx
@@ -24,38 +24,38 @@ export default function Room({ roomData }: { roomData: RoomData }) {
   }, [setAdmin]);
 
   return (
-    admin && (
-      <main className="bg-background flex flex-col gap-5 container mx-auto text-center py-48 sm:py-32 h-fit">
-        <section className="flex flex-col gap-4 mb-8">
-          <h1 className="text-5xl mb-4 font-bold">{roomData.residence_id}</h1>
-          <p className="font-semibold">Room: {roomData.roomNo}</p>
-          <p className="">{roomData.address}</p>
-        </section>
-        <section className="my-8 w-full">
-          <h1 className="text-xl font-semibold mb-8">Residents</h1>
-          <div className="flex gap-8 justify-center mx-auto">
-            {residents &&
-              residents.map((resident) => (
-                <Link
-                  href={`/room/${roomData.id}/residents/${resident.id}`}
-                  key={resident.resident_id}
-                  className="h-fit"
-                >
-                  <Card className="flex-col hover:bg-green-700/10 active:bg-green-700/10 flex shadow-md p-4 md:p-6 items-center md:w-[25vw] gap-4">
-                    <CardHeader className="p-0">
-                      <Image
-                        src="/profile.svg"
-                        alt="profile svg icon"
-                        className="border-4 border-black rounded-full"
-                        width={64}
-                        height={64}
-                      />
-                    </CardHeader>
-                    <CardContent className="grow p-0 flex flex-col justify-center h-3/5 text-left">
-                      <h3 className="capitalize items-center md:text-xl">
-                        {resident.resident_name}
-                      </h3>
-                    </CardContent>
+    <main className="bg-background flex flex-col gap-5 container mx-auto text-center py-48 sm:py-32 h-fit">
+      <section className="flex flex-col gap-4 mb-8">
+        <h1 className="text-5xl mb-4 font-bold">{roomData.residence_id}</h1>
+        <p className="font-semibold">Room: {roomData.roomNo}</p>
+        <p className="">{roomData.address}</p>
+      </section>
+      <section className="my-8 w-full">
+        <h1 className="text-xl font-semibold mb-8">Residents</h1>
+        <div className="flex gap-8 justify-center mx-auto">
+          {residents &&
+            residents.map((resident) => (
+              <Link
+                href={`/room/${roomData.id}/residents/${resident.id}`}
+                key={resident.resident_id}
+                className="h-fit"
+              >
+                <Card className="flex-col hover:bg-green-700/10 active:bg-green-700/10 flex shadow-md p-4 md:p-6 items-center md:w-[25vw] gap-4">
+                  <CardHeader className="p-0">
+                    <Image
+                      src="/profile.svg"
+                      alt="profile svg icon"
+                      className="border-4 border-black rounded-full"
+                      width={64}
+                      height={64}
+                    />
+                  </CardHeader>
+                  <CardContent className="grow p-0 flex flex-col justify-center h-3/5 text-left">
+                    <h3 className="capitalize items-center md:text-xl">
+                      {resident.resident_name}
+                    </h3>
+                  </CardContent>
+                  {admin && (
                     <CardFooter className="p-0">
                       <DeleteResident
                         {...{
@@ -64,11 +64,13 @@ export default function Room({ roomData }: { roomData: RoomData }) {
                         }}
                       />
                     </CardFooter>
-                  </Card>
-                </Link>
-              ))}
-          </div>
-        </section>
+                  )}
+                </Card>
+              </Link>
+            ))}
+        </div>
+      </section>
+      {admin && (
         <section className="mb-8 flex flex-col md:flex-row md:justify-center md:flex-wrap gap-6 w-full md:w-4/5 lg:w-2/3 mx-auto">
           <div className="flex gap-5 flex-wrap items-center justify-center md:w-2/3">
             <Button
@@ -81,7 +83,7 @@ export default function Room({ roomData }: { roomData: RoomData }) {
             </Button>
           </div>
         </section>
-      </main>
-    )
+      )}
+    </main>
   );
 }
