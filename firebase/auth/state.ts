8f2c5583e89292ad225eb08549d43b94d8f6b57c
Daniel Nyong <danielnyong66@gmail.com>
2025-04-07 21:30:03 +0100

    refactor

--- firebase/auth/state.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-export {};
-//import { onAuthStateChanged } from "firebase/auth";
-//import { auth } from "../client/config";
-//
-//onAuthStateChanged(auth, (admin) => {
-//  if (admin) console.log("user signed in: " + admin.email);
-//  else console.log("user signed out");
-//});
